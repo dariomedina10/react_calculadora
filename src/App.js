3c2e4edb27d71693b2a1a9e2bbb9c8b8fb72bc90
dariomedina10 <dariomedina10@gmail.com>
2025-02-02 02:29:05 -0400

    Estructura Calculadora

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
      <div className='freecodecamp-logo-contenedor'>
         <img src={logo} className='freecodecamp-logo' alt='logo' />
         </div>   
+        
+        <div className='contenedor-calculadora'>
+        <div className='fila'></div>  
+        <div className='fila'></div>  
+        <div className='fila'></div>  
+        <div className='fila'></div>  
+        <div className='fila'></div>  
+      
+       
+      
+        </div>   
     </div>
   );
 }
---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+//        <div className='fila'></div>} 
+function Boton(props){
+    
+    return(
+        
+        <div>
+        
+        </div>
+        
+    );
+}
