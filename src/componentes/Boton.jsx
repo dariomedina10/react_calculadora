d3a0672295f2193d116307ad4688030d51825822
dariomedina10 <dariomedina10@gmail.com>
2025-02-03 02:32:54 -0400

    Estructura Calculadora avanzada

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,12 +1,22 @@
-import react from 'react';
+import React from 'react';
+import '../hojas-de-estilo/Boton.css';
 //        <div className='fila'></div>} 
 function Boton(props){
     
+    const esOperador = valor =>{
+       
+       return isNaN (valor)  && (valor !== '.')   && (valor !== '=') 
+        
+    };
+    
     return(
         
-        <div>
-        
+        <div
+        className= {`boton-contenedor ${esOperador(props.children) ? 'operador' :null} `}>
+        {props.children} 
         </div>
         
     );
 }
+
+export default Boton;
